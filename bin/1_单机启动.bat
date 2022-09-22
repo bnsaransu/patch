d0234b423046e5188f4c41212246313a65b05550
bnsaransu <114031031+bnsaransu@users.noreply.github.com>
2022-09-22 13:20:03 -0300

    Delete bin directory

--- bin/1_单机启动.bat
+++
@@ -1,1 +0,0 @@
-Client.exe /sesskey /launchbylauncher /LoginMode 2 /ProxyIP:192.168.200.128
--- bin/Iniciar x32.bat
+++
@@ -1,1 +0,0 @@
-cmd /c start "" "Client.exe" /sesskey /launchbylauncher /LoginMode 2 /ProxyIP:177.55.48.104
